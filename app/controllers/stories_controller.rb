04d9a14156f1cab93a37a0b3f82594a0ddd9c722
paul manot <paulmanot@gmail.com>
2017-06-28 11:16:19 +1000

    Add stories controllerand sticky footer

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,26 @@
+class StoriesController < ApplicationController
+  def index
+    @stories = Story.all
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to stories_index_path
+      flash[:notice] = "Story Submission succeeded"
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+    def story_params
+      params.require(:story).permit(:name, :link)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get  'stories/index' => 'stories#index'
+  get  'stories/new'   => 'stories#new', as: 'new_story'
+  post 'stories'       => 'stories#create'
+
+  # resources :stories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
