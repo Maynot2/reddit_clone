9b3681a35b8fa0043e1bbb95c82d88dab1377b72
paul manot <paulmanot@gmail.com>
2017-07-06 11:45:13 +1000

    Add vote controller and Ajaxify vote behaviour

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  get  'stories'       => 'stories#index'
-  get  'stories/new'   => 'stories#new',    as: 'new_story'
-  post 'stories'       => 'stories#create'
-  get  'stories/:id'   => 'stories#show', as: 'story'
+  # get  'stories'       => 'stories#index'
+  # get  'stories/new'   => 'stories#new',    as: 'new_story'
+  # post 'stories'       => 'stories#create'
+  # get  'stories/:id'   => 'stories#show', as: 'story'
 
-  # resources :stories
+  resources :stories do
+    resources :votes
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
